fcfc81b4de70d017946836127442b530d9e4a489
天朝红雨 <178668028@qq.com>
2019-02-19 11:02:01 +0800

    修改包名

---
+++ xo-starter-ienum/src/test/java/com/github/jnoee/xo/ienum/TestEnum.java
@@ -0,0 +1,23 @@
+package com.github.jnoee.xo.ienum;
+
+public enum TestEnum implements IEnum {
+  ONE("一", "1"), TWO("二", "2");
+
+  private String text;
+  private String value;
+
+  private TestEnum(String text, String value) {
+    this.text = text;
+    this.value = value;
+  }
+
+  @Override
+  public String getText() {
+    return text;
+  }
+
+  @Override
+  public String getValue() {
+    return value;
+  }
+}
