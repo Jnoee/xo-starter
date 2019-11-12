124734bd6283ebe33a528b5a716b2103a9c8bb8f
hesong <hesong@cntest.com>
2019-11-12 10:52:56 +0800

    增加自定义类型声明。

--- xo-starter-ienum/src/main/java/com/github/jnoee/xo/ienum/usertype/package-info.java
+++ xo-starter-ienum/src/main/java/com/github/jnoee/xo/ienum/usertype/package-info.java
@@ -1,4 +1,5 @@
-@TypeDefs(value = {@TypeDef(name = "IEnum", typeClass = IEnumUserType.class)})
+@TypeDefs(value = {@TypeDef(name = "IEnum", typeClass = IEnumUserType.class),
+    @TypeDef(name = "IEnumList", typeClass = IEnumListUserType.class)})
 package com.github.jnoee.xo.ienum.usertype;
 
 import org.hibernate.annotations.TypeDef;
